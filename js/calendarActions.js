c1a136bbda6cadfa7d6f072a514cb8d834821e1a
Chad Moyer <chad.moyer@trumbullcorp.com>
2016-04-19 09:48:10 -0400

    added js to give us an array of just the weekdays selected by the user

--- js/calendarActions.js
+++ js/calendarActions.js
@@ -1,3 +1,17 @@
 /**
  * Created by chad on 4/18/16.
  */
+var getDates = function(startDate, endDate) {
+    var dates = [],
+        currentDate = startDate,
+        addDays = function(days) {
+            var date = new Date(this.valueOf());
+            date.setDate(date.getDate() + days);
+            return date;
+        };
+    while (currentDate <= endDate) {
+        dates.push(currentDate);
+        currentDate = addDays.call(currentDate, 1);
+    }
+    return dates;
+};
