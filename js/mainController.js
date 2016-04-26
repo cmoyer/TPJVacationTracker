01a3067ef134e2db9614793634f7929f71662a79
Chad Moyer <chad.moyer@trumbullcorp.com>
2016-04-26 11:03:30 -0400

    Added the basic http requests to get our user information and system defaults.

--- js/mainController.js
+++ js/mainController.js
@@ -7,20 +7,125 @@ var domainURL = '//www.pjdick.com';
 var dataURL = domainURL + '/VacationTracker.nsf';
 var recordLockURL = domainURL + '/VacationTracker.nsf/api/data/documents';
 var getLocksURL = domainURL + '/VacationTracker.nsf/api/data/collections/name/LockedRecords';
-var sysdefURL = domainURL + '/PNGRestoration.nsf/api/data/collections/name/SystemDefaults';
+var sysdefURL = domainURL + '/VacationTracker.nsf/api/data/collections/name/System Defaults';
 var dsMaxCount = 100; // the maximum count for the Domino data services
 
 main.controller('mainCtrl', function($scope, $http){
 
-    //custom functions added to scope
+    // custom functions added to scope
+    $scope.createVacationRequest = createVacationRequest;
+    $scope.logoutUser = logoutUser;
+    
+    // validation functions
 
-    //validation functions
-
-    //support functions
+    // support functions
 
 
+    //============================================================================
+    // BEGIN ANGULAR INITIALIZATION
+    //============================================================================
+    
+    // initialize our variables
+    initialize();
+    
+    function initialize(){
+        
+        // on load of the page, get the information for the currently authenticated user
+        //getUserData();
+        
+        // on load of the page, get the System Defaults data
+        //getSystemDefaults();
+        
+        
+    }
+
+    function getSystemDefaults() {
+        $http({
+            method: 'GET',
+            url: sysdefURL
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+            console.log("success");
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+            console.log("error");
+        });
+    }
+
+    function getUserData(){
+        $http({
+            method: 'GET',
+            url: dataURL + '/getLoginStatus?OpenPage'
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+        }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+        });
+    }
+
+    function lockDocument(){
+        if (!$scope.lockedByCurrentUser) {
+            var data = {
+                'lockedUNID': $rootScope.project.OBJECTID,
+                'lockedInfo1': $rootScope.project.jobNumber,
+                'lockedInfo2': $rootScope.project.activity,
+                'lockedRecordType': 'Project Information Sheet',
+                'lockedUser': $scope.user.username
+            };
+            console.log("locking");
+            if ($rootScope.project.OBJECTID) {
+                $http.post(recordLockURL + "?form=Locked%20Record", data).
+                then(function (response) {
+                    //console.log(response)
+                    $scope.lockedByCurrentUser = true;
+                });
+            }
+        }
+    }
+
+    function checkLock(objId){
+        $http.get(getLocksURL + "?keys=" + objId + "&keysexactmatch=true").
+        then(function(response){
+            var data = response.data;
+            if (data.length > 0){
+                if (data[0].LockedUser != $scope.user.username) {
+                    $scope.isLocked = true;
+                    $scope.lockedBy = data[0].LockedUser;
+                } else {
+                    $scope.lockedByCurrentUser = true;
+                }
+            }
+        });
+    }
+
+    function unlockDocument(objId){
+        if ($scope.lockedByCurrentUser) {
+            $http.get(getLocksURL + "?keys=" + objId + "&keysexactmatch=true").
+            then(function (response) {
+                console.log(response);
+                var data = response.data;
+                if (data.length > 0) {
+                    angular.forEach(data, function (value, key) {
+                        $http.delete(value['@href']).
+                        then(function (response) {
+                            //console.log(response);
+                        }, function (response) {
+                            console.log(response);
+                        });
+                    });
+                }
+            });
+        }
+    }
 
 
+    function createVacationRequest(){
+
+    }
 
 });
 
