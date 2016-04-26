a17500ccc8f85de5f785a239def9b13fd2d1fd45
Chad Moyer <chad.moyer@trumbullcorp.com>
2016-04-26 08:52:29 -0400

    added  service to our controller.

--- js/mainController.js
+++ js/mainController.js
@@ -5,8 +5,22 @@
 var main = angular.module('main', []);
 var domainURL = '//www.pjdick.com';
 var dataURL = domainURL + '/VacationTracker.nsf';
+var recordLockURL = domainURL + '/VacationTracker.nsf/api/data/documents';
+var getLocksURL = domainURL + '/VacationTracker.nsf/api/data/collections/name/LockedRecords';
+var sysdefURL = domainURL + '/PNGRestoration.nsf/api/data/collections/name/SystemDefaults';
+var dsMaxCount = 100; // the maximum count for the Domino data services
 
-main.controller('mainCtrl', function($scope){
+main.controller('mainCtrl', function($scope, $http){
+
+    //custom functions added to scope
+
+    //validation functions
+
+    //support functions
+
+
+
+
 
 });
 
