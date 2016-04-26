ae93a23637d88a99d89dce851b8f86bb5defea5f
Chad Moyer <chad.moyer@trumbullcorp.com>
2016-04-26 06:55:41 -0400

    got the main controller set up.

---
+++ js/mainController.js
@@ -0,0 +1,13 @@
+/**
+ * Created by chad on 4/25/16.
+ */
+
+var main = angular.module('main', []);
+var domainURL = '//www.pjdick.com';
+var dataURL = domainURL + '/VacationTracker.nsf';
+
+main.controller('mainCtrl', function($scope){
+
+});
+
+
